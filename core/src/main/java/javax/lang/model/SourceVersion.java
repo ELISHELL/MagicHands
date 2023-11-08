1dc3564e2a6ca389fc6610af66dc39365ade9889
1Plan <108533707+0cococ@users.noreply.github.com>
2023-11-08 20:32:22 +0800

    Add files via upload

---
+++ core/src/main/java/javax/lang/model/SourceVersion.java
@@ -0,0 +1,10 @@
+package javax.lang.model;
+//import  androidx.core
+public class SourceVersion {
+    public static SourceVersion latestSupported() {
+        return new SourceVersion();
+    }
+    public final int ordinal() {
+        return 8;
+    }
+}
